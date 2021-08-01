b4bc68cce2476c83880865db963e06d3018b8400
fisik-yum <govind.sriram@aol.com>
2021-08-01 13:57:16 -0700

    Initial commit.

---
+++ DATA/courses/planetpenguinracer/Candy_Lane/course.tcl
@@ -0,0 +1,10 @@
+tux_course_name "Candy Lane"
+tux_course_author "Soren Schimkat <www.schimkat.dk>"
+tux_course_dim 60 5000
+tux_start_pt 30 3.5
+tux_angle 35
+tux_elev_scale 10.0
+tux_base_height_value 255
+tux_elev elev.rgb
+tux_terrain terrain.rgb
+tux_course_init
---
+++ DATA/courses/tuxracer-belly-rub/Forest_River/course.tcl
@@ -0,0 +1,9 @@
+tux_course_name "Forest River"
+tux_course_author "http://tuxracer1.tripod.com/"
+tux_course_dim 120 280 120 250
+tux_start_pt 86 3
+tux_angle 19
+tux_elev_scale 3.5
+tux_elev elev.rgb
+tux_terrain terrain.rgb
+tux_course_init
